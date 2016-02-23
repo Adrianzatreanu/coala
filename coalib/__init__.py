3d18cc9dec2b947c0ac1faee05fc50edb9ce8bd5
adrian.zatreanu <zgadi_2007@yahoo.com>
2016-02-23 23:49:23 +0200

    coalib/__init__.py : Adding __version__ variable
    I have added the __version__ variable to
    check the version easier.

--- coalib/__init__.py
+++ coalib/__init__.py
@@ -3,6 +3,7 @@ import Constants
 
 __version__ = Constants.VERSION
 
+
 def assert_supported_version():  # pragma: no cover
     if not sys.version_info > (3, 2):
         print("coala supports only python 3.3 or later.")
